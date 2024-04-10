ee27dbe23e2b0106f03ce9e52ebd6ac3103b157e
Dean Chap <cchapotarong@mun.ca>
2024-04-10 16:08:28 -0600

    auth commit

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,9 +2,7 @@ import AppRouter from "next/dist/client/components/app-router";
 import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
-    anyApiRoute: publicProcedure.query(() => {
-        return 'hello'
-    }),
+    auth: authRouter
 })
 
 export type AppRouter = typeof appRouter
