28992c8975b5dacb55a6695f196c7c89ed39474f
Dean Chap <cchapotarong@mun.ca>
2024-04-22 19:56:30 -0600

    page commit

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -25,7 +25,7 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
-  const { data } = trpc.anyApiRoute.useQuery()
+  // const { data } = trpc.anyApiRoute.useQuery()
 
   const onSubmit = ({ 
     email, 
